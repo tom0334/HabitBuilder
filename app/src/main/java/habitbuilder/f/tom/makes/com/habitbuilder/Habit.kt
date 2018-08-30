e9725f5abfba8b0197ab503417286e71f7990118
tom0334 <tfriederich@live.nl>
2018-08-30 12:22:03 +0200

    add initial habit Class

---
+++ app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/Habit.kt
@@ -0,0 +1,47 @@
+package habitbuilder.f.tom.makes.com.habitbuilder
+
+
+data class HabitTimeStamp(val time: Long)
+
+interface HabitDatabase{
+    fun save(habit:Habit)
+    fun loadAll():List<Habit>
+    fun load(id:String):Habit
+}
+
+
+val SECONDS_IN_YEAR = 31556926
+
+class Habit(
+        val id:String,
+        var name: String,
+        var goal: Int,
+        val saver: HabitDatabase
+        ){
+    private val timeStamps = mutableListOf<HabitTimeStamp>()
+
+    fun checkData(){
+        timeStamps.sortBy { it.time }
+    }
+
+    fun addTimeStamp(stamp:HabitTimeStamp){
+        timeStamps.add(stamp)
+        checkData()
+    }
+
+    fun scoreUptoTimeStamp(upTo:Long):Float{
+        if(timeStamps.size==0) return 0F
+
+        var count = 0
+        for (stamp in this.timeStamps){
+           if(stamp.time < upTo){
+               count++
+           }
+        }
+
+        val first = timeStamps[0].time
+        val passedDays=  (upTo - first) /SECONDS_IN_YEAR
+        return count.toFloat() / goal.toFloat() * passedDays.toFloat()
+    }
+
+}
