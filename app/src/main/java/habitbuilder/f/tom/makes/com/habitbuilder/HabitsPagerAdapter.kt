80333dd3a4a56ae8c96d8394ef38cf46e936acfe
tom0334 <tfriederich@live.nl>
2018-09-01 12:34:30 +0200

    refractor mainactivty and habitspageradapter

--- app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/HabitsPagerAdapter.kt
+++ app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/HabitsPagerAdapter.kt
@@ -5,18 +5,18 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 
 
-class HabitsPagerAdapter(fm: FragmentManager, private val habits: List<Habit>): FragmentStatePagerAdapter(fm) {
+class HabitsPagerAdapter(fm: FragmentManager, var data: List<Habit>): FragmentStatePagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
-        assert(position in habits.indices)
-        return HabitFrag.newInstance(position, habits[position].id)
+        assert(position in data.indices)
+        return HabitFrag.newInstance(position, data[position].id)
     }
 
     override fun getCount(): Int {
-        return habits.size
+        return data.size
     }
 
     override fun getPageTitle(position: Int): CharSequence? {
-        return "Habit $position"
+        return data[position].name
     }
 }
--- app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/MainActivity.kt
+++ app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/MainActivity.kt
@@ -7,35 +7,38 @@ import android.support.v4.view.ViewPager
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var saver: HabitDatabase
+    private lateinit var adapter:HabitsPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        this.saver = SnappyHabitSaver(this)
 
-        val saver = SnappyHabitSaver(this)
+        //prepare the viewpager
+        this.adapter =  HabitsPagerAdapter(this.supportFragmentManager, saver.loadAll())
+        val pager = findViewById<ViewPager>(R.id.mainPager)
+        pager.adapter = adapter
+
+        val tabLayout = findViewById<TabLayout>(R.id.mainTabLayout)
+        tabLayout.setupWithViewPager(pager)
+    }
+
+    private fun refresh(){
+        val newData = saver.loadAll()
+        adapter.data = newData
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun addHabit(){
         val habit = Habit(
                 saver.generateNewHabitId(),
                 "Test Habit",
                 3
         )
         saver.save(habit)
-
-
-        val all = saver.loadAll()
-
-        //prepare the viewpager
-        val adapter = HabitsPagerAdapter(this.supportFragmentManager, all)
-        val pager = findViewById<ViewPager>(R.id.mainPager)
-        pager.adapter = adapter
-
-        val tabLayout = findViewById<TabLayout>(R.id.mainTabLayout)
-        tabLayout.setupWithViewPager(pager)
-
-
-
-
-
-
     }
 
 }
