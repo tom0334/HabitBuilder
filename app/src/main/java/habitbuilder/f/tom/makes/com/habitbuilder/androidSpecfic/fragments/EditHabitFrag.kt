0c7eba54e2d0264b64fcbeb848a194ac81bd2a76
tom0334 <tfriederich@live.nl>
2018-10-21 15:07:26 +0200

    createHabit: switch to onCreateView from onCreateDialog

--- app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/androidSpecfic/fragments/EditHabitFrag.kt
+++ app/src/main/java/habitbuilder/f/tom/makes/com/habitbuilder/androidSpecfic/fragments/EditHabitFrag.kt
@@ -1,19 +1,20 @@
 package habitbuilder.f.tom.makes.com.habitbuilder.androidSpecfic.fragments
 
-import android.app.Dialog
+
 import android.os.Bundle
 import android.support.v7.app.AppCompatDialogFragment
 import habitbuilder.f.tom.makes.com.habitbuilder.R
 import habitbuilder.f.tom.makes.com.habitbuilder.androidSpecfic.PARAM_ONE_ID
-import android.app.AlertDialog
 import android.content.Context
+import android.view.LayoutInflater
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import habitbuilder.f.tom.makes.com.habitbuilder.androidSpecfic.HabitCreatorListener
 import habitbuilder.f.tom.makes.com.habitbuilder.common.Habit
-import kotlinx.android.synthetic.main.frag_create_habit.view.*
 import android.widget.Toast
+import kotlinx.android.synthetic.main.frag_create_habit.*
 
 
 /**
@@ -40,10 +41,7 @@ class EditHabitFrag : AppCompatDialogFragment(){
             return f
         }
     }
-    //The content view of the dialog. THE NORMAL VIEW FIELD IN FRAGMENT DOES NOT WORK HERE
-    //that is caused by the fact that this is a dialogFragment, and the view is managed by the
-    //dialog. I cannot set it since it is final. Ugh.
-    lateinit var CV: View
+
 
     //A callback to the activity that will save the habit once it is constructed.
     lateinit var callback: HabitCreatorListener
@@ -69,64 +67,46 @@ class EditHabitFrag : AppCompatDialogFragment(){
         }
     }
 
-
-    /**
-     * This function is used instead of the normal onCreateView(), because it adds a title and
-     * the two save and discard buttons in the material style easily.
-     */
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val inflater = activity!!.layoutInflater
-        val builder = AlertDialog.Builder(activity)
-
-        val view = inflater.inflate(R.layout.frag_create_habit, null)
-        val dialog = builder.setTitle("Create new Habit")
-                .setView(view)
-                .setPositiveButton("Save",null)
-                .setNegativeButton("Discard") { dialog, id ->
-                    callback.onDone(null)
-                    //The fragment is discarded automatically
-                }.create()
-
-        setupView(view)
-
-        //Overide the normal onclick of the positive button to only dismiss if the user has
-        //typed in all fields.
-        dialog.setOnShowListener {
-            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                val hab = parseHabit()
-                if(hab!=null){
-                    callback.onDone(hab)
-                    dialog.dismiss()
-                }
-                //else the parseHabit function will show a toast indicating what the user needs
-                //to do to create a valid habit.
-            }
-        }
-        return dialog
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.frag_create_habit, container)
     }
 
-
     /**
-     * This initializes the contentView of the dialog, and sets the CV field in this class to
-     * refer to that contentView.
-     *
-     * It changes the text of the goal explanation corresponding to if the habit is positive
+     * Initializes the view of this fragment, and sets up the click listeners for the buttons
      */
-    private fun setupView(CV: View) {
-        this.CV = CV
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        createHabit_button_save.setOnClickListener {
+            val hab = parseHabit()
+            if(hab!=null){
+                callback.onDone(hab)
+                dialog.dismiss()
+            }
+            //else the parseHabit function will show a toast indicating what the user needs
+            //to do to create a valid habit.
+        }
+
+        createHabit_button_discard.setOnClickListener {
+            callback.onDone(null)
+            dialog.dismiss()
+        }
+
 
         //check the positive habit by default
-        CV.createHabit_pos_radioButton.isChecked = true
+        createHabit_pos_radioButton.isChecked = true
 
-        CV.createHabit_posNeg_radioGroup.setOnCheckedChangeListener { _ , _ ->
-            val goalTv = CV.createHabit_goalText
-            if (CV.createHabit_pos_radioButton.isChecked) {
+        createHabit_posNeg_radioGroup.setOnCheckedChangeListener { _ , _ ->
+            val goalTv = createHabit_goalText
+            if (createHabit_pos_radioButton.isChecked) {
                 goalTv.text = getString(R.string.createHabitFrag_goalAtLeast)
             } else {
                 goalTv.text = getString(R.string.createHabitFrag_GoalLessThan)
             }
         }
     }
+
+
 
     /**
      * Parses the views of this dialog to create a new habit. It can return null if not all
@@ -149,23 +129,23 @@ class EditHabitFrag : AppCompatDialogFragment(){
             }
         }
 
-        val name = CV.createHabit_habitName.text.toString()
+        val name = createHabit_habitName.text.toString()
         if (name.isEmpty()) {
-            showError("Name is empty!")
+            showError(getString(R.string.createHabitFrag_toast_error_name))
             return null
         }
 
         val goal = try {
-            CV.createHabit_goalEditText.parseTextOrHint()
+            createHabit_goalEditText.parseTextOrHint()
         } catch (e: NumberFormatException) {
-            showError("Invalid number input")
+            showError(getString(R.string.createHabitFrag_toast_error_goal))
             return null
         }
 
         val goalDays = try {
-            CV.createHabit_goalTimespanEditText.parseTextOrHint()
+            createHabit_goalTimespanEditText.parseTextOrHint()
         }catch (e: NumberFormatException){
-            showError("Invalid number input!")
+            showError(getString(R.string.createHabitFrag_toast_error_goalDays))
             return null
         }
 
@@ -174,7 +154,7 @@ class EditHabitFrag : AppCompatDialogFragment(){
                 name = name,
                 goal = goal,
                 goalDays =goalDays,
-                positive = CV.createHabit_pos_radioButton.isChecked
+                positive = createHabit_pos_radioButton.isChecked
         )
     }
 
